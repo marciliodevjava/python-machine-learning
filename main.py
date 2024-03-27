55d0da1c1426603bd79e6d651842727f6a3c344d
marciliodevjava <marciliodevjava@gmail.com>
2024-03-26 21:26:17 -0300

    Update main

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import pandas as pd
+import plotly.express as px
 
 dados = pd.read_csv('marketing_investimento.csv')
 
 print(dados)
+
+aderencia_investimento = px.histogram(dados, x='aderencia_investimento', text_auto=True)
+
+aderencia_investimento.show()
+
+estado_civil = px.histogram(dados, x='estado_civil', text_auto=True, color='aderencia_investimento', barmode='group')
+
+estado_civil.show()
