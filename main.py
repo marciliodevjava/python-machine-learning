9d08da6a5e7aeb56be00095696f9dd453960d09e
marciliodevjava <marciliodevjava@gmail.com>
2024-03-26 21:29:52 -0300

    Update main

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ dados = pd.read_csv('marketing_investimento.csv')
 print(dados)
 
 aderencia_investimento = px.histogram(dados, x='aderencia_investimento', text_auto=True)
-
 aderencia_investimento.show()
 
 estado_civil = px.histogram(dados, x='estado_civil', text_auto=True, color='aderencia_investimento', barmode='group')
-
 estado_civil.show()
+
+escolaridade = px.histogram(dados, x='escolaridade', text_auto=True, color='aderencia_investimento', barmode='group')
+escolaridade.show()
+
